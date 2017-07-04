95c6c736ec9bb7c5710c6b4513091fc5d0b585ff
Isaac Lozano <109lozanoi@gmail.com>
2017-07-04 14:24:28 -0700

    sa2_set: added serde support

--- sa2_set/src/lib.rs
+++ sa2_set/src/lib.rs
@@ -1,13 +1,14 @@
 extern crate byteorder;
+#[macro_use] extern crate serde_derive;
 
 use std::io::{self, Read, Write};
 
 use byteorder::{ReadBytesExt, WriteBytesExt, ByteOrder, LittleEndian, BigEndian};
 
-#[derive(Clone,Copy,Debug)]
+#[derive(Clone,Copy,Debug,Serialize,Deserialize)]
 pub struct Object(pub u16);
 
-#[derive(Clone,Copy,Debug)]
+#[derive(Clone,Copy,Debug,Serialize,Deserialize)]
 pub struct Rotation {
     pub x: u16,
     pub y: u16,
@@ -41,7 +42,7 @@ impl Rotation {
     }
 }
 
-#[derive(Clone,Copy,Debug)]
+#[derive(Clone,Copy,Debug,Serialize,Deserialize)]
 pub struct Position {
     pub x: f32,
     pub y: f32,
@@ -75,7 +76,7 @@ impl Position {
     }
 }
 
-#[derive(Clone,Copy,Debug)]
+#[derive(Clone,Copy,Debug,Serialize,Deserialize)]
 pub struct SetObject {
     pub object: Object,
     pub rotation: Rotation,
@@ -121,7 +122,7 @@ impl SetObject {
     }
 }
 
-#[derive(Clone,Debug)]
+#[derive(Clone,Debug,Serialize,Deserialize)]
 pub struct SetFile(pub Vec<SetObject>);
 
 impl SetFile {
